446b2c44f5a2a02603fcff0c419c6f9c72ebefcf
Kavya P S <kavya.ps095@gmail.com>
2020-07-26 09:41:31 +0530

    Implementing databinding for Main Activity

--- AIDL Client/app/src/main/java/com/kavya/aidl_client/MainActivity.kt
+++ AIDL Client/app/src/main/java/com/kavya/aidl_client/MainActivity.kt
@@ -2,10 +2,14 @@ package com.kavya.aidl_client
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.kavya.aidl_client.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
